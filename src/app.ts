05114fc73f69da9e5b3c6dd006b0b7b9c07c6a02
Alvaro Sena <alvarosenacs.c@gmail.com>
2026-02-16 11:08:32 -0300

    feat: add create poll endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import express from "express";
+import { z } from "zod";
+import { db } from "./db";
+import { polls } from "./db/schema";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "hello, world" });
+app.post("/polls", async (request, response) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  try {
+    const { title } = createPollBody.parse(request.body);
+
+    const [poll] = await db.insert(polls).values({ title }).returning();
+
+    return response.status(201).json({ pollId: poll.id });
+
+  } catch {}
 });
 
 const port = process.env.PORT || 8080;
